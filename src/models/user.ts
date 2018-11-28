c3b5d6308c397d3d4780aca057a8236179989375
chenkf <chenkf@umich.edu>
2018-11-27 22:55:40 -0500

    added some CSS; created userdata model

---
+++ src/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+	id: number;
+    name: string;
+    company: string;
+    availability: boolean;
+    img: string;
+    bio: string;
+}
--- src/pages/user-list/user-list.ts
+++ src/pages/user-list/user-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { UserProfilePage } from '../user-profile/user-profile';
+import { User } from '../../models/user';
 
 /**
  * Generated class for the UserListPage page.
@@ -15,14 +16,51 @@ import { UserProfilePage } from '../user-profile/user-profile';
   templateUrl: 'user-list.html',
 })
 export class UserListPage {
-
+  private users: User[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	this.loadFakeEntries();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserListPage');
   }
 
+  private loadFakeEntries() {
+  	this.users = [
+      {
+        id: 1,
+	    name: "Woody",
+	    company: "Google",
+	    availability: 1,
+	    img: "assets/imgs/avatar-ts-woody.png",
+	    bio: "How are you?",
+      },
+      {
+        id: 2,
+	    name: "Buzz Lightyear",
+	    company: "Amazon",
+	    availability: 0,
+	    img: "assets/imgs/avatar-ts-buzz.png",
+	    bio: "Bonjour!",
+      },
+      {
+        id: 3,
+	    name: "Jessie",
+	    company: "Microsoft",
+	    availability: 1,
+	    img: "assets/imgs/avatar-ts-jessie.png",
+	    bio: "Hello",
+      },
+      {
+        id: 4,
+	    name: "Mr. Potato Head",
+	    company: "Uber",
+	    availability: 1,
+	    img: "assets/imgs/avatar-ts-potatohead.png",
+	    bio: "Have you had a meal today?",
+      }];      
+
+  }
   public jump() {
   	this.navCtrl.push(UserProfilePage); 
   }
