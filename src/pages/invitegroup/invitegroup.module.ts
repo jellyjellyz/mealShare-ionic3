19de3f216091bb9c53d758d01e3084c9ce5555c9
Guanchao Huang <guanchaohuang@Guanchaos-MacBook-Pro.local>
2018-12-01 11:55:39 -0500

    commit

---
+++ src/pages/invitegroup/invitegroup.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InvitegroupPage } from './invitegroup';
+
+@NgModule({
+  declarations: [
+    InvitegroupPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(InvitegroupPage),
+  ],
+})
+export class InvitegroupPageModule {}
