974af6eeea17ce6464273318103b2a666c25b1f4
Jiayu Zhang <jellyjellychris@gmail.com>
2018-12-09 21:29:04 -0500

    remove unused code

--- src/pages/create-event/create-event.ts
+++ src/pages/create-event/create-event.ts
@@ -135,7 +135,6 @@ export class CreateEventPage {
 
       this.event.key = this.eventDataService.addEvent(this.event);
 
-      console.log("in add event get new key", this.eventDataService.addEvent(this.event));
       this.sendMessage();
       this.navCtrl.pop();
     }
